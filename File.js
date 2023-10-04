8cfba76198b2030ade4b1587880e46d8b982322b
Sumayah Alw <Sarah141592@gmail.com>
2023-10-04 19:10:01 +0300

    change

---
+++ File.js
@@ -0,0 +1,17 @@
+
+function message()
+{ alert ("This alert box was called with the onload event")}
+
+function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+  }
+
+function myFunction2() {
+    var x = document.getElementById("lname");
+    x.value = x.value.toUpperCase();
+  }
+
+function chang(x) {
+    document.getElementById(x).style.background = "Ivory";
+  }
